6925e5580384dcfe7954b6a1f5226fb0d4a72f92
Filip Osoianu <filip.osoianu@gmail.com>
2020-03-23 13:04:32 +0200

    update aggreagator

--- lib/aggregator.ex
+++ lib/aggregator.ex
@@ -13,8 +13,14 @@ defmodule Aggregator do
 
     state = %{
       time: start_time,
-      final_sensors_value: %{},
-      mean_sensors_value: %{},
+      sensor_value_list: %{
+        temperature_sensor: [],
+        humidity_sensor: [],
+        atmo_pressure_sensor: [],
+        wind_speed_sensor: [],
+        light_sensor: []
+      },
+      final_sensor_value: %{},
       forecast: final_forecast,
       forecast_list: forecast_list
     }
@@ -24,93 +30,112 @@ defmodule Aggregator do
 
   @impl true
   def handle_call(:get_forecast, _from, state) do
-    forecast = %{
-      forecast: state[:forecast],
-      temperature_sensor: state[:final_sensors_value][:temperature_sensor],
-      humidity_sensor: state[:final_sensors_value][:humidity_sensor],
-      atmo_pressure_sensor: state[:final_sensors_value][:atmo_pressure_sensor],
-      wind_speed_sensor: state[:final_sensors_value][:wind_speed_sensor],
-      light_sensor: state[:final_sensors_value][:light_sensor]
-    }
+    # forecast = %{
+    #   forecast: state[:forecast],
+    #   temperature_sensor: state[:sensor_value_list][:temperature_sensor],
+    #   humidity_sensor: state[:sensor_value_list][:humidity_sensor],
+    #   atmo_pressure_sensor: state[:sensor_value_list][:atmo_pressure_sensor],
+    #   wind_speed_sensor: state[:sensor_value_list][:wind_speed_sensor],
+    #   light_sensor: state[:sensor_value_list][:light_sensor]
+    # }
 
-    {:reply, forecast, state}
+    {:reply, state, state}
   end
 
   @impl true
   def handle_cast({:forecast, forecast, current_senors_value}, state) do
-    start_time = state[:time]
     time_now = Time.utc_now()
-
+    start_time = state[:time]
     diff = Time.diff(time_now, start_time, :millisecond)
-
-    final_sensors_value = state[:final_sensors_value]
-    mean_sensors_value = state[:mean_sensors_value]
+    sensor_value_list = state[:sensor_value_list]
+    final_sensor_value = state[:final_sensor_value]
     forecast_list = state[:forecast_list]
     final_forecast = state[:final_forecast]
 
     if diff > 1000 do
-      final_sensors_value = mean_sensors_value
-      final_forecast = most_common(forecast_list)
-      forcecast_list = []
 
+      final_forecast = most_frequent(forecast_list)
+      forcecast_list = []
+      final_sensor_value = get_average_value(sensor_value_list)
+      sensor_value_list = %{
+        temperature_sensor: [],
+        humidity_sensor: [],
+        atmo_pressure_sensor: [],
+        wind_speed_sensor: [],
+        light_sensor: []
+      }
       state = %{
         time: time_now,
-        final_sensors_value: final_sensors_value,
-        mean_sensors_value: mean_sensors_value,
         forecast: final_forecast,
-        forecast_list: forcecast_list
+        forecast_list: forcecast_list,
+        final_sensor_value: final_sensor_value,
+        sensor_value_list: sensor_value_list
       }
 
-      IO.puts("<------------------------------->")
-      IO.puts("Forecast")
-      IO.inspect(state[:forecast])
-      IO.puts("Atmosphere pressure ")
-      IO.inspect(state[:final_sensors_value][:atmo_pressure_sensor])
-      IO.puts("Humidity")
-      IO.inspect(state[:final_sensors_value][:humidity_sensor])
-      IO.puts("Light")
-      IO.inspect(state[:final_sensors_value][:light_sensor])
-      IO.puts("Temperature")
-      IO.inspect(state[:final_sensors_value][:temperature_sensor])
-      IO.puts("Wind speed")
-      IO.inspect(state[:final_sensors_value][:wind_speed_sensor])
-      IO.puts("<------------------------------->")
+      IO.inspect(print(state))
 
       {:noreply, state}
     else
-      mean_sensors_value = get_sensor_mean_value(mean_sensors_value, current_senors_value)
-
       state = %{
         time: start_time,
-        final_sensors_value: final_sensors_value,
-        mean_sensors_value: mean_sensors_value,
         forecast: final_forecast,
-        forecast_list: [forecast | forecast_list]
+        forecast_list: [forecast | forecast_list],
+        final_sensor_value: final_sensor_value,
+        sensor_value_list: add_value_in_map(sensor_value_list, current_senors_value)
       }
 
       {:noreply, state}
     end
   end
 
-  def most_common(list) do
-    map = Enum.frequencies(list)
-    list_values = Map.values(map)
-    max_value = Enum.max(list_values)
-
-    forecast =
-      Enum.map(map, fn {k, v} ->
-        if v == max_value do
-          k
-        end
-      end)
-
-    [head | _tail] = Enum.filter(forecast, &(!is_nil(&1)))
-    head
+  def print(state) do
+    IO.puts("<------------------------------->")
+    IO.puts("Forecast")
+    IO.inspect(state[:forecast])
+    IO.puts("Atmosphere pressure ")
+    IO.inspect(state[:final_sensor_value][:atmo_pressure_sensor])
+    IO.puts("Humidity")
+    IO.inspect(state[:final_sensor_value][:humidity_sensor])
+    IO.puts("Light")
+    IO.inspect(state[:final_sensor_value][:light_sensor])
+    IO.puts("Temperature")
+    IO.inspect(state[:final_sensor_value][:temperature_sensor])
+    IO.puts("Wind speed")
+    IO.inspect(state[:final_sensor_value][:wind_speed_sensor])
+    IO.puts("<------------------------------->")
   end
 
-  def get_sensor_mean_value(current_sensors_value_map, mean_sensors_value_map) do
-    Map.merge(current_sensors_value_map, mean_sensors_value_map, fn _k, v1, v2 ->
-      (v1 + v2) / 2
+
+  def add_value_in_map(map_of_list, map_of_values) do
+    map =
+      Enum.reduce(map_of_list, map_of_list, fn {k, v}, acc ->
+        value = map_of_values[k]
+        v = [value | v]
+        Map.put(acc, k, v)
+      end)
+
+    map
+  end
+
+  def get_average_value(map_of_list) do
+    avgList =
+      Enum.map(map_of_list, fn {key, val} ->
+        {key, [Enum.reduce(val, fn score, sum -> sum + score end) / Enum.count(val)]}
+      end)
+
+    map = Enum.into(avgList, %{})
+
+    Enum.reduce(map, map, fn {k, v}, acc ->
+      [head | _tail] = v
+      Map.put(acc, k, head)
     end)
+  end
+
+  def most_frequent(list) do
+    map = Enum.frequencies(list)
+    map = Enum.sort(map, fn {_k, v}, {_k1, v1} -> v > v1 end)
+    tuple = Enum.at(map, 0)
+    list = Tuple.to_list(tuple)
+    List.first(list)
   end
 end
